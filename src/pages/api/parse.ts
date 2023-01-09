7370ef26cab4988645770d27a9eca801fc60804e
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-01-09 01:08:31 +0100

    chore: fix format import

--- src/pages/api/parse.ts
+++ src/pages/api/parse.ts
@@ -1,13 +1,31 @@
 //! WE NEED TO IMPORT OS BECAUSE ELSE NEXTJS IS NOT INCLUDING OAS INSIDE THE BUNDLE(PRODUCTION BUILD)
 import oas from 'oas'
+import { format as prettierFormat } from 'prettier'
 
 import type { File } from '@kubb/core'
-import { build, format } from '@kubb/core'
+import { build } from '@kubb/core'
 import createSwagger from '@kubb/swagger'
 import createSwaggerTypescript from '@kubb/swagger-typescript'
 import createSwaggerReactQuery from '@kubb/swagger-react-query'
 
 import type { NextApiRequest, NextApiResponse } from 'next'
+import type { Options } from 'prettier'
+
+const formatOptions: Options = {
+  tabWidth: 2,
+  printWidth: 160,
+  parser: 'typescript',
+  singleQuote: true,
+  semi: false,
+  bracketSameLine: false,
+  endOfLine: 'auto',
+}
+export const format = (text?: string) => {
+  if (!text) {
+    return undefined
+  }
+  return prettierFormat(text, formatOptions)
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
