bb7d91565addaf0dd787d2e1a8a5e2db50461f2d
Stijn Van Hulle <stijn@stijnvanhulle.be>
2024-03-10 22:09:13 +0100

    chore: use of Vercel blob storage instead of s3

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,14 +1,22 @@
-import { uploadObject } from '../../aws'
+import { put } from '@vercel/blob'
+import crypto from 'node:crypto'
 
 import type { NextApiRequest, NextApiResponse } from 'next'
+
+export const config = { matcher: '/upload' }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     if (req.method === 'POST') {
       const { body } = req
-      const signedUrl = await uploadObject(body.input)
 
-      res.status(200).json({ url: signedUrl })
+      const blob = await put(crypto.randomUUID(), body.input, {
+        access: 'public',
+        // 1 hour
+        cacheControlMaxAge: 60 * 60,
+      })
+
+      res.status(200).json({ url: blob.downloadUrl })
       return
     }
     res.status(200).send(undefined)
